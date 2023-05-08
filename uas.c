c26a2ffc311a2b5df35bfd1910ef783bc9b61345
Keyzia Vania Elsya <132829674+keyziAaa@users.noreply.github.com>
2023-05-08 11:52:08 +0700

    Update uas.c

--- uas.c
+++ uas.c
@@ -28,19 +28,41 @@ int main()
         printf("-----------------------------\n");
         printf("Pilihan : ");
         scanf("%d", &pilihan);
-switch (pilihan)
+        
+        switch (pilihan)
         {
         case 1:
             if (jumlahBuku < 50)
             {
                 struct Buku buku;
-                printf("\nJudul Buku : ");
+                printf("\nJudul Buku              : ");
                 scanf(" %[^\n]s", buku.judul);
-                printf("Pengarang Buku : ");
+                printf("Pengarang Buku          : ");
                 scanf(" %[^\n]s", buku.pengarang);
-                printf("Penerbit Buku : ");
+                printf("Penerbit Buku           : ");
                 scanf(" %[^\n]s", buku.penerbit);
-                printf("Tahun Terbit Buku : ");
+                printf("Tahun Terbit Buku       : ");
                 scanf("%d", &buku.tahunTerbit);
-                printf("Jumlah Halaman Buku : ");
+                printf("Jumlah Halaman Buku     : ");
                 scanf("%d", &buku.halaman);
+                buku.dipinjam = 0;
+                daftarBuku[jumlahBuku] = buku;
+                jumlahBuku++;
+                FILE *file = fopen("data_perpustakaan.txt", "w");
+                if (file)
+                {
+                    for (int i = 0; i < jumlahBuku; i++)
+                    {
+                        fprintf(file, "%s|%s|%s|%d|%d|%s\n", daftarBuku[i].judul, daftarBuku[i].pengarang,
+                                daftarBuku[i].penerbit, daftarBuku[i].tahunTerbit, daftarBuku[i].halaman,
+                                daftarBuku[i].dipinjam ? "Dipinjam" : "Tersedia");
+                    }
+                    fclose(file);
+                }
+                printf("\nBuku berhasil ditambahkan.\n\n");
+            }
+            else
+            {
+                printf("\nGagal menambahkan buku karena telah penuh.\n\n");
+            }
+            break;
